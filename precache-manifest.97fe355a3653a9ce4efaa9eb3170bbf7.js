db1da3844460beced31d70ccb9c35ac40d008287
Daniil <daniel.eshenko@gmail.com>
2020-09-13 02:20:42 +0300

    Updates

---
+++ precache-manifest.97fe355a3653a9ce4efaa9eb3170bbf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a885eed0fb69c09ed4d68d7b19c02f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a7315fd9bdc666fc7d",
+    "url": "/static/css/2.571efafb.chunk.css"
+  },
+  {
+    "revision": "9ce7acf30e441259d899",
+    "url": "/static/css/main.6c357515.chunk.css"
+  },
+  {
+    "revision": "94a7315fd9bdc666fc7d",
+    "url": "/static/js/2.fd4b6b17.chunk.js"
+  },
+  {
+    "revision": "9ce7acf30e441259d899",
+    "url": "/static/js/main.1f6af028.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
